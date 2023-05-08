9c2d326ac8331cf2e69327163a2c5f3fe70fc5fc
fwiko <raff@raffsimms.com>
2023-05-08 16:19:19 +0100

    Added ability to have results output to a CSV file using --output toggle flag (default; false).

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"net"
@@ -13,15 +14,17 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"golang.org/x/exp/slices"
 )
 
 type Options struct {
 	TargetAddress string
-	WordlistPath  string
+	WordListPath  string
 	WorkerCount   int
 	StatusCodes   []int
+	Output        bool
 }
 
 func isValidUrl(targetAddress string) bool {
@@ -40,7 +43,7 @@ func isValidUrl(targetAddress string) bool {
 	return err == nil
 }
 
-func loadWordlist(wordlistPath string) ([]string, error) {
+func loadWordList(wordlistPath string) ([]string, error) {
 	wordlistFile, err := os.Open(wordlistPath)
 	if err != nil {
 		return make([]string, 0), err
@@ -64,20 +67,33 @@ func requestUrl(targetAddress string) (int, error) {
 	return resp.StatusCode, nil
 }
 
-func leftJustify(text string, length int) string {
-	return fmt.Sprintf("%s%s", text, strings.Repeat(" ", length-len(text)))
+func outputResults(results [][]string) (int, error) {
+	outputFile, err := os.Create(fmt.Sprintf("results_%d.csv", time.Now().Unix()))
+	if err != nil {
+		return 0, err
+	}
+	defer outputFile.Close()
+
+	writer := csv.NewWriter(outputFile)
+	defer writer.Flush()
+
+	header := []string{"URL", "Status"}
+	writer.Write(header)
+	writer.WriteAll(results)
+
+	return 0, nil
 }
 
 func main() {
 	options := Options{}
 
 	flag.StringVar(&options.TargetAddress, "target", "", "Target address to scan")
-	flag.StringVar(&options.WordlistPath, "wordlist", "", "The path of the wordlist file to use")
+	flag.StringVar(&options.WordListPath, "wordlist", "", "The path of the wordlist file to use")
 	flag.IntVar(&options.WorkerCount, "workers", runtime.NumCPU(), "Number of threads to use (default; CPU thread count)")
+	flag.BoolVar(&options.Output, "output", false, "Output results to a file (default; false)")
 
 	tempStatusCodes := flag.String("status-codes", "200,301,302,303,307", "Comma-separated list of status codes to look for")
 	options.StatusCodes = make([]int, len(strings.Split(*tempStatusCodes, ",")))
-
 	for i, code := range strings.Split(*tempStatusCodes, ",") {
 		options.StatusCodes[i], _ = strconv.Atoi(strings.Trim(code, " "))
 	}
@@ -93,13 +109,14 @@ func main() {
 		options.TargetAddress = (options.TargetAddress)[:len(options.TargetAddress)-1]
 	}
 
-	if options.WordlistPath == "" {
+	if options.WordListPath == "" {
 		fmt.Println("* Please specify a wordlist path using the --wordlist flag")
 		return
 	}
-	wordlist, err := loadWordlist(options.WordlistPath)
+
+	wordlist, err := loadWordList(options.WordListPath)
 	if err != nil {
-		fmt.Printf("* Unable to open wordlist file: %s\n", options.WordlistPath)
+		fmt.Printf("* Unable to open wordlist file: %s\n", options.WordListPath)
 		return
 	}
 
@@ -109,12 +126,14 @@ func main() {
 	fmt.Printf(
 		"\n* %s\n* %s\n* %s\n\n",
 		fmt.Sprintf("Target..... %s", options.TargetAddress),
-		fmt.Sprintf("Wordlist... %s", filepath.Base(options.WordlistPath)),
+		fmt.Sprintf("Wordlist... %s", filepath.Base(options.WordListPath)),
 		fmt.Sprintf("Threads.... %d", options.WorkerCount),
 	)
 
 	matches := 0
 	checked := 0
+
+	results := [][]string{}
 
 	for i := 0; i < options.WorkerCount; i++ {
 		wg.Add(1)
@@ -122,14 +141,22 @@ func main() {
 			defer wg.Done()
 			for word := range in {
 				t := options.TargetAddress + "/" + word
+
 				statusCode, err := requestUrl(t)
 				if err != nil {
 					continue
 				}
+
 				if slices.Contains(options.StatusCodes, statusCode) {
 					fmt.Printf("[%d] %s\n", statusCode, t)
 					matches++
 				}
+
+				if options.Output {
+					r := []string{t, strconv.Itoa(statusCode)}
+					results = append(results, r)
+				}
+
 				checked++
 				if checked != len(wordlist) {
 					fmt.Fprintf(os.Stdout, "* %d/%d Checked, %d Matches\r", checked, len(wordlist), matches)
@@ -145,4 +172,8 @@ func main() {
 	}
 	close(in)
 	wg.Wait()
+
+	if options.Output {
+		outputResults(results)
+	}
 }
